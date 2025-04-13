75ca93b85f08f44ebce9b78d9a5389cf0db65339
eomjinyoung <jinyoung.eom@gmail.com>
2025-04-13 23:01:43 +0900

    .

--- hooks/useUserInfo.js
+++ hooks/useUserInfo.js
@@ -10,7 +10,7 @@ export const useUserInfo = () => {
   const [userInfo, setUserInfo] = useState(null);
 
   useEffect(() => {
-    setToken(Cookies.getItem("jwt_token"));
+    setToken(Cookies.get("jwt_token"));
   });
 
   useEffect(() => {
