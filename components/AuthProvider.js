f35bda7af691ea2bb74466d1030792f271c2b303
eomjinyoung <jinyoung.eom@gmail.com>
2025-04-15 17:41:47 +0900

    .

--- components/AuthProvider.js
+++ components/AuthProvider.js
@@ -11,12 +11,6 @@ export const AuthProvider = ({ children }) => {
   const setToken = useCallback((token) => {
     console.log("setToken() 호출됨!");
 
-    if (!token) {
-      console.log("토큰이 없습니다.");
-      resetToken();
-      return;
-    }
-
     const fetchUserInfo = async () => {
       try {
         const response = await fetch(
